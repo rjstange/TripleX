c92cd9d124f05b70524a5487dd08322f00904de0
Riley Stange <riley.stange@gmail.com>
2019-12-08 11:21:40 -0600

    Cleaned up the string outputs and edited some text. Placed the gameplay loop in the new PlayGame function.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,17 +1,14 @@
 #include <iostream>
 
-int main()
+void PlayGame()
 {
     //Print introduction messages to the terminal
-    std::cout << "You wake up in a world in a strange place composed of geometric shapes.";
-    std::cout << std::endl;
-    std::cout << "A complex geometric form approaches you and you hear it speak in your mind:";
-    std::cout << std::endl;
-    std::cout << "\"I am Hedronax, the Math Lord. You are in The Hyperplane.\"";
-    std::cout << std::endl;
-    std::cout << "\"All that lives here is perfect math. To prove you belong here, I will ask you to solve riddles of math.\"";
-    std::cout << std::endl;
-    std::cout << "\"Answer wrongly, even once, and you will cease to exist!\"" << std::endl;
+    std::cout << "\nYou wake up in a strange place composed entirely of geometric shapes.\n";
+    std::cout << "A complex geometric form approaches you and you hear it speak in your mind:\n\n";
+
+    std::cout << "\"I am Hedronax, the Math Lord. You are in The Hyperplane.\"\n";
+    std::cout << "\"All that lives here is perfect math. To prove you belong here, I will ask you to solve riddles of math.\"\n";
+    std::cout << "\"Answer wrongly, even once, and you will be banished!\"\n\n";
 
     // Declare 3 number code
     const int CodeA = 4;
@@ -22,29 +19,31 @@ int main()
     const int CodeProduct = CodeA * CodeB * CodeC;
     
     // Print sum and product to the terminal
-    std::cout << std::endl;
-    std::cout << "There are three numbers in the code." << std::endl;
-    std::cout << "The codes add up to: " << CodeSum << std::endl;
-    std::cout << "The codes multiply to give: " << CodeProduct << std::endl;
+    std::cout << "\"There are three numbers in the code.\"\n";
+    std::cout << "\"The codes add up to: " << CodeSum << "\"\n";
+    std::cout << "\"And multiply to give: " << CodeProduct << "\"" << std::endl;
 
+    // Store player guesses and receive the input
     int GuessA, GuessB, GuessC;
     
-    std::cin >> GuessA;
-    std::cin >> GuessB;
-    std::cin >> GuessC;
+    std::cin >> GuessA >> GuessB >> GuessC;
 
     int GuessSum = GuessA + GuessB + GuessC;
     int GuessProduct = GuessA * GuessB * GuessC;
 
+    // Check if the player's guess is correct or not
     if (GuessSum == CodeSum && GuessProduct == CodeProduct)
     {
-        std::cout << "Correct! You are worthy to dwell in The Hyperplane!";
+        std::cout << "\n\"Correct! You are worthy to dwell in The Hyperplane!\"\n";
     }
-
     else
     {
-        std::cout << "Pathetic, you are banished from my realm!";
+        std::cout << "\n\"Pathetic, you are banished from my realm!\"\n";
     }
+}
 
+int main()
+{
+    PlayGame();
     return 0;
 }
