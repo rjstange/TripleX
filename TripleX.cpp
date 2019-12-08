59ac6955d106e1ba33e970a25e1babd1254cfd37
Riley Stange <riley.stange@gmail.com>
2019-12-08 12:09:10 -0600

    Added a LevelDifficulty variable in the main so the level difficulty can be stored and tracked. Added an if statement for PrintIntroduction to play a different intro message for the first level, and then the same one for all subsequent levels.

--- TripleX.cpp
+++ TripleX.cpp
@@ -1,19 +1,28 @@
 #include <iostream>
 
-void PrintIntroduction()
+void PrintIntroduction(int Difficulty)
 {
-    //Print introduction messages to the terminal
-    std::cout << "\nYou wake up in a strange place composed entirely of geometric shapes.\n";
-    std::cout << "A complex geometric form approaches you and you hear it speak in your mind:\n\n";
+    // First level introduction
+    if (Difficulty == 1)
+    {
+        // Print introduction messages to the terminal
+        std::cout << "\nYou wake up in a strange place composed entirely of geometric shapes.\n";
+        std::cout << "A complex geometric form approaches you and you hear it speak in your mind:\n\n";
 
-    std::cout << "\"I am Hedronax, the Math Lord. You are in The Hyperplane.\"\n";
-    std::cout << "\"All that lives here is perfect math. To prove you belong here, I will ask you to solve riddles of math.\"\n";
-    std::cout << "\"Answer wrongly, even once, and you will be banished!\"\n\n";
+        std::cout << "\"I am Hedronax, the Math Lord. You are in The Hyperplane.\"\n";
+        std::cout << "\"All that lives here is perfect math. To prove you belong here, I will ask you to solve riddles of math.\"\n";
+        std::cout << "\"Answer wrongly, even once, and you will be banished!\"\n\n";
+    }
+    else
+    {
+        std::cout << "\"Well done! Now for the next question.\"\n"
+        std::cout << "\"Remember, if you answer wrongly, you will be banished!\"\n\n"
+    }
 }
 
-bool PlayGame()
+bool PlayGame(int Difficulty)
 {
-    PrintIntroduction();
+    PrintIntroduction(Difficulty);
     // Declare 3 number code
     const int CodeA = 4;
     const int CodeB = 8;
@@ -23,8 +32,9 @@ bool PlayGame()
     const int CodeProduct = CodeA * CodeB * CodeC;
     
     // Print sum and product to the terminal
-    std::cout << "\"There are three numbers in the code.\"\n";
-    std::cout << "\"The codes add up to: " << CodeSum << "\"\n";
+    std::cout << "\"Riddle #" << Difficulty << ":\"\n";
+    std::cout << "\"There are three numbers:\"\n";
+    std::cout << "\"They add up to: " << CodeSum << "\"\n";
     std::cout << "\"And multiply to give: " << CodeProduct << "\"" << std::endl;
 
     // Store player guesses and receive the input
@@ -50,12 +60,19 @@ bool PlayGame()
 
 int main()
 {
+    int LevelDifficulty = 1;
+
     while(true)
     {
-        bool bLevelComplete = PlayGame();
-        PlayGame();
-        std::cin.clear();
-        std::cin.ignore();
+        bool bLevelComplete = PlayGame(LevelDifficulty);
+        std::cin.clear(); // Clears any errors
+        std::cin.ignore(); // Discards the buffer
+        
+        if (bLevelComplete)
+        {
+            ++LevelDifficulty;
+        }
+        
     }
     
     return 0;
